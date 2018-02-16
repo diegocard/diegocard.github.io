ae7da73b01668a14621993c483852e5a839860b7
Diego Cardozo <diegocard@gmail.com>
2018-02-16 02:28:36 -0300

    Concatenated and minified CSS and JS

--- steamprofile/gulpfile.js
+++ steamprofile/gulpfile.js
@@ -5,12 +5,39 @@ let uglify = require('gulp-uglify');
 let plumber = require('gulp-plumber');
 let rename = require('gulp-rename');
 let clean = require('gulp-clean');
-let cleanCSS = require('gulp-clean-css');
+let uglifyCSS = require('gulp-uglifycss');
 let connect = require('gulp-connect');
 let watch = require('gulp-watch');
 
+let cssFiles = './css/*.css';
+let jsFiles = [
+	'./javascript/prototype-1.7.js',
+	'./javascript/_combined.js',
+	'./javascript/global.js',
+	'./javascript/jquery-1.11.1.min.js',
+	'./javascript/tooltip.js',
+	'./javascript/shared_global.js',
+	'./javascript/jquery_init.js',
+	'./javascript/modalContent.js',
+	'./javascript/modalv2.js',
+	'./javascript/profile.js',
+	'./javascript/stickers.js',
+	'./javascript/reportedcontent.js',
+	'./javascript/shared_responsive_adapter.js',
+	'./javascript/*.js'
+]
+
+gulp.task('css', () => {
+	return gulp.src(cssFiles)
+		.pipe(sourcemaps.init())
+		.pipe(concat('app.min.css'))
+		.pipe(uglifyCSS({compatibility: 'ie8'}))
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest('dist'));
+});
+
 gulp.task('js', () => {
-	return gulp.src('./javascript/*.js')
+	return gulp.src(jsFiles)
 		.pipe(sourcemaps.init())
 		.pipe(plumber())
 		.pipe(concat('app.js'))
@@ -22,15 +49,6 @@ gulp.task('js', () => {
 		.pipe(sourcemaps.write('maps'))
 		.pipe(gulp.dest('dist'))
 });
-
-gulp.task('css', () => {
-	return gulp.src('css/*.css')
-		.pipe(sourcemaps.init())
-		.pipe(concat('app.min.css'))
-		.pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(sourcemaps.write('maps'))
-		.pipe(gulp.dest('dist'));
-  });
 
 gulp.task('clean', () => {
     return gulp.src('dist', {read: false})
---
+++ steamprofile/javascript/jquery_init.js
@@ -0,0 +1,4 @@
+$J = jQuery.noConflict();
+if ( typeof JSON != 'object' || !JSON.stringify || !JSON.parse ) {
+    document.write( "<scr" + "ipt type=\"text\/javascript\" src=\"http:\/\/community.edgecast.steamstatic.com\/public\/javascript\/json2.js?v=pmScf4470EZP&amp;l=english\" ><\/script>\n" );
+};
