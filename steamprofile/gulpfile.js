ba60b92eab341538e03f2af4489cd5f88d2df2b8
Diego Cardozo <diegocard@gmail.com>
2018-02-16 01:53:12 -0300

    Finished working on gulp workflow

--- steamprofile/gulpfile.js
+++ steamprofile/gulpfile.js
@@ -1,21 +1,54 @@
-var gulp = require('gulp');
-var minifyCSS = require('gulp-csso');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
+let gulp = require('gulp');
+let concat = require('gulp-concat');
+let sourcemaps = require('gulp-sourcemaps');
+let uglify = require('gulp-uglify');
+let plumber = require('gulp-plumber');
+let rename = require('gulp-rename');
+let clean = require('gulp-clean');
+let cleanCSS = require('gulp-clean-css');
+let connect = require('gulp-connect');
+let watch = require('gulp-watch');
 
-gulp.task('css', function () {
-	return gulp.src('css/*.css')
-		.pipe(concat('app.min.css'))
-		.pipe(minifyCSS())
-		.pipe(gulp.dest('css'))
-});
-
-gulp.task('js', function () {
-	return gulp.src('client/javascript/*.js')
+gulp.task('js', () => {
+	return gulp.src('./javascript/*.js')
 		.pipe(sourcemaps.init())
-		.pipe(concat('app.min.js'))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('javascript'))
+		.pipe(plumber())
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(uglify())
+		.pipe(rename({
+			extname: '.min.js'
+		}))
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', ['css', 'js']);
+gulp.task('css', () => {
+	return gulp.src('css/*.css')
+		.pipe(sourcemaps.init())
+		.pipe(concat('app.min.css'))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest('dist'));
+  });
+
+gulp.task('clean', () => {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('server', () => {
+	return connect.server({
+		root: './',
+		livereload: true
+	});
+});
+
+gulp.task('watch-and-reload', ['build'], () => {
+	watch(['javascript/**','css/**','./*.html'], () => {
+		gulp.start('build');
+	}).pipe(connect.reload());
+});
+
+gulp.task('build', ['clean', 'css', 'js']);
+gulp.task('watch', ['build', 'watch-and-reload', 'server']);
