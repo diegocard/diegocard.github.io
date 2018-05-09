e5467abb93d20da9542bc3747e7469b955677d7d
Diego Cardozo <diegocard@gmail.com>
2018-05-09 02:27:41 -0300

    First functional serviceworker

--- steamprofile/service-worker.js
+++ steamprofile/service-worker.js
@@ -1,30 +1,43 @@
+var dataCacheName = 'dc-steamprofile';
+var cacheName = 'dc-steamprofilea';
+var filesToCache = [
+	'/',
+	"/index.html",
+	"/dist/app.min.js",
+	"/dist/app.min.css",
+	"/service-worker.js"
+];
+
 self.addEventListener('install', function(e) {
+	console.log('[ServiceWorker] Install');
 	e.waitUntil(
-		caches.open('diegocard').then(function(cache) {
-			return cache.addAll([
-				'/',
-				'/steamprofile',
-				'/index.html',
-				'/steamprofile/index.html',
-				'dist/app.min.js',
-				'dist/app.min.css'
-			]);
+		caches.open(cacheName).then(function(cache) {
+			console.log('[ServiceWorker] Caching assets');
+			return cache.addAll(filesToCache);
 		})
 	);
 });
 
-self.addEventListener('activate', function(event) {
-	event.waitUntil(self.clients.claim());
-});
-  
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.open('diegocard')
-		.then(function(cache) {
-			cache.match(event.request, {ignoreSearch: true})
+self.addEventListener('activate', function(e) {
+	console.log('[ServiceWorker] Activate');
+	e.waitUntil(
+		caches.keys().then(function(keyList) {
+			return Promise.all(keyList.map(function(key) {
+				if (key !== cacheName && key !== dataCacheName) {
+					console.log('[ServiceWorker] Removing old cache', key);
+					return caches.delete(key);
+				}
+			}));
 		})
-		.then(function(response) {
-			return response || fetch(event.request);
+	);
+	return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+	console.log('[Service Worker] Fetch', e.request.url);
+	e.respondWith(
+		caches.match(e.request).then(function(response) {
+			return response || fetch(e.request);
 		})
 	);
 });
